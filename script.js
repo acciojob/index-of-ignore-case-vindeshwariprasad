011cc28c5be00402d6471640eb666b04c4896ada
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-03-26 04:43:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	const lowerStr = str.toLowerCase();
+    const lowerSubStr = subStr.toLowerCase();
+    
+    // Find the index of the substring in the string
+    return lowerStr.indexOf(lowerSubStr);
 }
 
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
